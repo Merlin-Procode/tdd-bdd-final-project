a1b375e793981082c1dfc430abad822869dd74c3
Merlin-Procode <you@example.com>
2025-01-28 06:14:39 -0500

    finished

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -32,6 +32,7 @@ from service import app
 from service.common import status
 from service.models import db, init_db, Product
 from tests.factories import ProductFactory
+from urllib.parse import quote_plus  # Add this import
 
 # Disable all but critical errors during normal test run
 # uncomment for debugging failing tests
